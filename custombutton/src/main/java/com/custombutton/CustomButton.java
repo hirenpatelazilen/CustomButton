7b466c5059f2b161fc2066f41cedab97e1c20856
hiren patel <hiren.patel@azilen.com>
2017-02-12 21:16:43 +0530

    Initial commit

---
+++ custombutton/src/main/java/com/custombutton/CustomButton.java
@@ -0,0 +1,27 @@
+package com.custombutton;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Created by hiren patel on 2/12/2017.
+ */
+
+public class CustomButton extends Button {
+    public CustomButton(Context context) {
+        super(context);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
